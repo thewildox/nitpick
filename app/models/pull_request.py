1f91c21a7ace6a0df8d5f07dcd45e0cbf8744cfd
Eselasie <eselasieakpablie2005@gmail.com>
2026-07-09 17:18:25 -0700

    Cleaned code

--- app/models/pull_request.py
+++ app/models/pull_request.py
@@ -1,14 +1,18 @@
 from datetime import datetime
-from sqlalchemy import BigInteger, ForeignKey, String, func
+
+from sqlalchemy import ForeignKey, String, UniqueConstraint, func
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.models.base import Base
 
+
 class PullRequest(Base):
     __tablename__ = "pull_requests"
+    __table_args__ = (UniqueConstraint("repository_id", "pr_number"),)
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    pr_number: Mapped[int] = mapped_column(ForeignKey("repositories.id"))
+    repository_id: Mapped[int] = mapped_column(ForeignKey("repositories.id"))
+    pr_number: Mapped[int] = mapped_column(index=True)
     title: Mapped[str] = mapped_column(String(255))
     author: Mapped[str] = mapped_column(String(255))
     head_sha: Mapped[str] = mapped_column(String(40))
